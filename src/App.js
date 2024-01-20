12ebe2f33247e8639976677ea5bb6459bf534258
Pranil-Meherkar <pranil@nimapinfotech.com>
2024-01-20 21:47:31 +0530

    added code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [fcount, setFcount] = useState(localStorage.getItem("fcount") || 0);
+  const [ecount, setEcount] = useState(localStorage.getItem("ecount") || 0);
+
+  const temp = { "Fresher Interviews": fcount, "Experienced Interviews": ecount }
+
+  const increment = (type) => () => {
+    type === "Fresher Interviews" ?
+    setFcount(prev => prev + 1):
+    setEcount(prev => prev + 1)
+
+    localStorage.setItem(
+      type === "Fresher Interviews" ? "fcount" : "ecount",
+      type === "Fresher Interviews" ? fcount + 1 : ecount + 1,
+    )
+  }
+
+  const decrement = (type) => () => {
+    type === "Fresher Interviews" ?
+    setFcount(prev => prev - 1):
+    setEcount(prev => prev - 1)
+
+    localStorage.setItem(
+      type === "Fresher Interviews" ? "fcount" : "ecount",
+      type === "Fresher Interviews" ? fcount - 1 : ecount - 1,
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Object.entries(temp).map(row => {
+        return <div className='card'>
+          <p>{row[0]} : {row[1]}</p>
+          <span className='button-container'>
+            <button className='button' onClick={increment(row[0])}>+</button>
+            <button className='button' onClick={decrement(row[0])}>-</button>
+          </span>
+        </div>
+      })}
     </div>
   );
 }
